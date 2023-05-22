e36c8bcbe3a20e47cb6093f1e822724b579db282
Clarry30 <133214496+Clarry30@users.noreply.github.com>
2023-05-22 10:31:59 +0000

    Create clarence.py

---
+++ clarence.py
@@ -0,0 +1,22 @@
+#My Azubi Story
+a = "Hello World, "
+b = "this is my journey with Azubi Africa."
+print(a + b)
+
+#the story
+my_story = ["a Cloud Trainee", "Spring23 cohort"]
+username = "My name is Clarence."
+print(username)
+
+intro = "I am"
+print(f"{intro} {(my_story[0])} with the {my_story[1]}.")
+body = """My experience has been fine, albeit overwhelming.
+But I am determined to learn and succeed."""
+print(body)
+
+#rpc game
+c = "let's play a game!"
+print(c)
+
+moves = ("rock", "paper", "scissors")
+user_move = ()
